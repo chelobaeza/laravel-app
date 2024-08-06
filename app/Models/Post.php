6901c512b199260ac3ef710f0d43aa373efce6b4
Marcelo Baeza <marcelo.baeza@endava.com>
2024-08-06 18:31:11 -0300

    update Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,5 +9,10 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title',];
+    protected $fillable = [
+        'id',
+        'title',
+        'created_at',
+        'updated_at'
+    ];
 }
